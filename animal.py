0c8e8fb067b69c960a49597b25c1fa683ed8d5a9
anywhere27 <ada_hack217@hackademy.mx>
2022-03-24 23:56:56 -0600

    muestra un animal, completado

--- animal.py
+++ animal.py
@@ -8,8 +8,8 @@ from animales import tiburon, serpiente, paloma, leon
 #clase
 menu = {
    
-   '1': '1. tiburon,',
-   '2': '2. serpientes',
+   '1': '1. tiburon',
+   '2': '2. serpiente',
    '3': '3. paloma',
    '4': '4. leon',
    '0': '0. exit'
@@ -17,29 +17,25 @@ menu = {
    
 class Animal():
    def __init__(self):
-      self.type = type
+      self.tiburon = tiburon
+      self.serpiente = serpiente
+      self.paloma = paloma
+      self.leon = leon
       pass
    
-   def mostrar_animal(self):
-      
-      animal_art = getattr(animales, self.type)
+   def mostrar_animal(self, animal):
+      animal_art = getattr(animales, animal)
       return animal_art
       pass
-   
-   
-mostrar = animales
-mostrar.menu = tiburon
-mostrar.menu = serpiente
-mostrar.menu = paloma
-mostrar.menu = leon
-
 
 # ejecucion principal
 
 if __name__ == '__main__':
    for option in menu:
       print(menu.get(option))
-       
+   animal = Animal()   
+   
+   
    while True:
       user_input = input('ingresa una opcion: ')
       if user_input == '0':
@@ -47,10 +43,10 @@ if __name__ == '__main__':
          break
       elif user_input in menu:
          menu_value = menu.get(user_input)
-         print(menu_value.split(' '))
+         print(menu_value.split(' ')[1])
          #animal =Animal()
          
-         print("mostrar", animales.menu)
+         print("mostrar", animal.mostrar_animal(menu_value.split(' ')[1]))
          pass
       else:
          print("invalido")
